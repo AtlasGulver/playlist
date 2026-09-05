c0d30fade4c01fe2a64a7fa32a7a29dece0f1fe0
AtlasGulver <brccetinkaya82@gmail.com>
2026-09-05 19:59:30 +0300

    playlist.py

---
+++ playlist.py
@@ -0,0 +1,22 @@
+playlist = {
+    "Cem Karaca" : "Bidanem",
+    "Shakira" : "Dai Dai",
+    "Pepee": "İki Ekmek Aldım",
+    "Imagine Dragons" : "Believer",
+    "Alper Eğri" : "Democratik Kongo",
+    "Shakira" : "Waka Waka",
+    "Teoman(Ayrılsakta Beraberiz)" : "O je ne pas si vous plait de"
+}
+
+for i in playlist.keys():
+  print(i)
+
+print("((((((((((((((((((((((((((((((((((((((()))))))))))))))))))))))))))))))))))))))")
+
+for sarki in playlist.values():
+  print(sarki)
+
+print("((((((((((((((((((((((((((((((((((((((()))))))))))))))))))))))))))))))))))))))")
+
+for hepsi in playlist.items():
+  print(hepsi)
